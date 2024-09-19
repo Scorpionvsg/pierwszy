4fe062bceafe9fafcabb53913271932dc8e1c6a4
Scorpionvsg <jeremiasz20088@gmail.com>
2024-09-19 16:58:42 +0200

    liczenie

--- liczenie.php
+++ liczenie.php
@@ -9,7 +9,19 @@
     <?php
     $x=5;
     $y=15;
-    echo "Wynik dodawania $a oraz $b =".$a+$b;
+    echo "Wynik dodawania $x oraz $y = ".$x+$y;?><br>
+    <?php echo "Wynik odejmowania $x oraz $y = ".$x-$y;?><br>
+    <?php echo "Wynik mnożenia $x oraz $y = ".$x*$y;?><br>
+    <?php echo "Wynik dzielenia $x oraz $y = ".$x/$y;?><br>
+    <?php echo "Wynik reszty z dzielenia $x oraz $y = ".$x % $y;?><br>
+<hr>
+    <?php 
+    $a=4;
+    $b=35;
+    echo "Obwód prostokąta o bokach długości $a oraz $b jest równy: ".(2*$a + 2*$b);?>
+    <?php echo "Pole prostokąta o bokach długosci $a oraz $b jest równe: ".$a*$b;?> 
+    j<sup >2</sup>
+
     
     
 </body>
