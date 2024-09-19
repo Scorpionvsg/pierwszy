2a8c24417f9cbc6ea81e3b0c110a94942d6a3b0d
Scorpionvsg <jeremiasz20088@gmail.com>
2024-09-19 17:19:34 +0200

    lekcje php dobrze

--- lekcja 19.09/miesiac.php
+++ lekcja 19.09/miesiac.php
@@ -4,9 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Zad3-miesiąc</title>
+    <style>
+        #h1{
+            border-width: 4px; 
+            border-style: solid;
+            border-color: pink;
+            aligh
+        }
+    </style>
 </head>
 <body>
     <?php
-    $miesiąc=rand(1,15);?>
+    $miesiąc=rand(1,15);
+echo "<h1><center>$miesiąc</center></h1>";?>
+
+
 </body>
 </html>
