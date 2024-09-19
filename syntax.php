76fda5635c11678787a4be07ac2f5f6cd1a23b56
Scorpionvsg <jeremiasz20088@gmail.com>
2024-09-19 16:11:45 +0200

    lekcja nr.1

---
+++ syntax.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>syntax</title>
+</head>
+<body>
+    <?php
+        echo "Tu "."można "."pisać "."różnie";
+        print("Zgłasza sie funkcja print");
+        print 'Znów zgłasza sie funkcja'.' print';?>
+        <?= "edytor"." tekstowy";
+//tu komantarze
+#różnego rodzaju
+
+/*wielo
+lub 
+jedno
+liniowe
+
+*/
+
+</body>
+</html>
