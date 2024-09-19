975ea075418916f1ba0a630c5fc523c442785b01
Scorpionvsg <jeremiasz20088@gmail.com>
2024-09-19 16:48:09 +0200

    puierwsze

---
+++ liczenie.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LICZENIE</title>
+</head>
+<body>
+    <?php
+    $x=5;
+    $y=15;
+    echo "Wynik dodawania $a oraz $b =".$a+$b;
+    
+    
+</body>
+</html>
