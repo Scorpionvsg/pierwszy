459d40d11857c1300a6e1f8be475261d4aef04da
Scorpionvsg <Jeremiasz20088@gmail.com>
2024-09-19 21:22:04 +0200

    koniec

--- lekcja 19.09/miesiac.php
+++ lekcja 19.09/miesiac.php
@@ -7,9 +7,16 @@
     <style>
         #h1{
             border-width: 4px; 
-            border-style: solid;
+            border-style: dotted;
             border-color: pink;
-            aligh
+            text-align: center
+        }
+        #h3{
+            border-width: 2px; 
+            border-style: dashed;
+            border-color: pink;
+            text-align: center ;
+            font-family: cursive;
         }
     </style>
 </head>
@@ -17,7 +24,24 @@
     <?php
     $miesiąc=rand(1,15);?>
 <h1><?=echo "<center>$miesiąc</center">?></h1>;
-
-
+<h3><?php
+    switch($miesiąc){
+        case 1,2,3:
+            echo "I Kwartał";
+            break;
+        case 4,5,6:
+            echo "II Kwartał";
+            break;
+        case 7,8,9:
+            echo "III Kwartał";
+            break;
+        case 10,11,12:
+            echo "IV Kwartał";
+            break;
+        default: 
+            echo"Błędny numer miesiąca";
+            break;
+    }?>
+</h3>
 </body>
 </html>
