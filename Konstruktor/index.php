42635e90a0fb20db8a40c4171162b842ac334786
Scorpionvsg <jeremiasz20088@gmail.com>
2024-11-07 17:08:27 +0100

    dg

--- Konstruktor/index.php
+++ Konstruktor/index.php
@@ -18,12 +18,12 @@
     <?php 
     $pom=new Arytmetyka(0.005,0.002);
     ?>
-    <li>  Suma liczb <b><?php echo$pom->x?></b> i <b><?php echo$pom->y?></b> wynosi <b><?php echo suma();?></b></li>
-    <li>  Różnica liczb <b><?php echo$pom->x?></b> i <b><?php echo$pom->y?></b> wynosi <b><?php echo różnica();?></b></li>
-    <li>  Iloczyn liczb <b><?php echo$pom->x?></b> i <b><?php echo$pom->y?></b> wynosi <b><?php echo iloczyn();?></b></li>
-    <li>  Iloraz liczb <b><?php echo$pom->x?></b> i <b><?php echo$pom->y?></b> wynosi <b><?php echo iloraz();?></b></li>
-    <li>  Reszta z dzielenia liczb <b><?php echo$pom->x?></b> i <b><?php echo$pom->y?></b> wynosi <b><?php echo reszta();?></b></li>
-    <li>  Liczba <b><?php echo$pom->x?></b> do potęgi <b><?php echo$pom->y?></b> wynosi <b><?php echo potęga();?></b></li>
+    <li>  Suma liczb <b><?php echo$pom->x?></b> i <b><?php echo$pom->y?></b> wynosi <b><?php echo $pom->suma();?></b></li>
+    <li>  Różnica liczb <b><?php echo$pom->x?></b> i <b><?php echo$pom->y?></b> wynosi <b><?php echo $pom->roznica();?></b></li>
+    <li>  Iloczyn liczb <b><?php echo$pom->x?></b> i <b><?php echo$pom->y?></b> wynosi <b><?php echo $pom->iloczyn();?></b></li>
+    <li>  Iloraz liczb <b><?php echo$pom->x?></b> i <b><?php echo$pom->y?></b> wynosi <b><?php echo $pom->iloraz();?></b></li>
+    <li>  Reszta z dzielenia liczb <b><?php echo$pom->x?></b> i <b><?php echo$pom->y?></b> wynosi <b><?php echo $pom->reszta();?></b></li>
+    <li>  Liczba <b><?php echo$pom->x?></b> do potęgi <b><?php echo$pom->y?></b> wynosi <b><?php echo $pom->potega();?></b></li>
 </ul>
 </body>
 </html>
