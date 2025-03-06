9fcfce20d17a6cfb39b505160675d45b5197bff7
Scorpionvsg <jeremiasz20088@gmail.com>
2025-03-06 16:49:36 +0100

    im

--- cookies.php/Panel_admin_cook.php
+++ cookies.php/Panel_admin_cook.php
@@ -6,16 +6,44 @@ session_start();?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Panel Administracyjny</title>
+    <title>Ustawianie cisteczka</title>
+    <style>
+        body{
+            text-align: center;
+            margin:30px;
+        }
+        </style>
 </head>
 <body>
     <?php include "menu_cook.php";?>
-    <h1>Panel administracyjny</h1>
+    <h1>Ustaw Ciastko</h1>
     <?php
-    if(isset($_SESSION["login_status"]) && $_SESSION["login_status"]){
-        echo "Jesteś zalogowany jako ". $_SESSION['login'] ;
+    if(isset($_SESSION["login_status"]) && $_SESSION["login_status"]){?>
+    <form method="post">
+        <fieldset>
+            <legend>Nazwa ciasteczka</legend>
+            <input type="text" id="nazc" name="nazc" readonly value="good_password_definition">
+        </fieldset>
+        <fieldset>
+            <legend>Wygasa:(w sekundach)</legend>
+            <input type="number" id="czas" name="czas" min="0">
+        </fieldset>
+        <fieldset>
+            <legend>Ustaw</legend>
+            <input type="submit" id="ust" name="ust" value="ustaw">
+        </fieldset>
+        <fieldset>
+            <legend>Zawartość</legend>
+            <textarea id="zaw" name="zaw" value="zawartość" autofocus></textarea>
+        </fieldset>
+    </form>
+     <?php
+     if(isset($_POST["ustaw"])){
+       
+     }
+
     }else{
-        echo "Nie jestes jeszcze zalogowany";
+        echo "Musisz być zalogowany, aby ustawić ciastko";
     }  
      include "stopka_cook.php";?>
 </body>
